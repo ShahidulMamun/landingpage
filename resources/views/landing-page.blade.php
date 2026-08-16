53b3044895aa18cea8d872d165a5e96106d50965
ShahidulMamun <mdshahidulcse@gmail.com>
2026-08-16 13:57:40 +0600

    css lading page css added

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -14,7 +14,7 @@
 <!-- Icons -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <!-- Custom -->
-<link rel="stylesheet" href="{{ asset('css/landing.css') }}">
+<link rel="stylesheet" href="{{ asset('css/landing.page.css') }}">
 </head>
 <body>
 
