1daad29549c2d181a5180da6361608f7bfddf974
ShahidulMamun <mdshahidulcse@gmail.com>
2026-08-16 15:41:26 +0600

    order Route added

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingPageController;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', [LandingPageController::class, 'index'])->name('landing');
+Route::post('/order', [OrderController::class, 'store'])->name('order.store');
