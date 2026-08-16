b5eb6a07a4c319fcc24977521ca90c8073c05f3f
ShahidulMamun <mdshahidulcse@gmail.com>
2026-08-16 18:12:04 +0600

    category and product crud route added in web

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
+
+
 
 Route::get('/', [LandingPageController::class, 'index'])->name('landing');
 Route::post('/order', [OrderController::class, 'store'])->name('order.store');
+
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+});
