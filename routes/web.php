84de8e762f0bc1f26ba99e4e35d39e4e164304b4
ShahidulMamun <mdshahidulcse@gmail.com>
2026-08-17 09:27:42 +0600

    dashboard route added in web

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\DashboardController;
 
 
 
@@ -14,6 +15,7 @@ Route::post('/order', [OrderController::class, 'store'])->name('order.store');
 
 
 Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('categories', CategoryController::class);
     Route::resource('products', ProductController::class);
 });
