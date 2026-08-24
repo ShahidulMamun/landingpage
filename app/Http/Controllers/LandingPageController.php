4b6a20cdac4a6468d93e5582674d4858352ccc3f
ShahidulMamun <mdshahidulcse@gmail.com>
2026-08-24 09:51:45 +0600

    LandingController update

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Admin\Category;
 
 class LandingPageController extends Controller
 {
     public function index(){
-
-        return view('landing');
+        
+        $categories = Category::where('is_active',true)->orderBy('sort_order','ASC')->get();
+        return view('landing',compact('categpries'));
     }
 }
